9ba4cab253487e979b05b9437f3b35b03fce0457
Alexander Chebotin <globaltouareg@gmail.com>
2022-04-10 17:35:58 +0300

    Добавлен производный класс "активная клетка"

--- cell.h
+++ cell.h
@@ -2,7 +2,38 @@
 #include "checker.h"
 
 //КЛАСС КЛЕТКА
+//Это просто пустая клетка, неактивная, не может иметь шашку. (это будут светлые клетки)
 class Cell {
+public:
+	virtual void drawCell(); //рисовка клетки (Просто закрашенный квадрат)
+	//(Тк этот класс является клеткой неактивной, поэтому большинство функций - ничего не делают)
+	virtual void selectChecker() {}
+	void selectCheckerOFF() {}
+
+	virtual void makePossibleMove() {}
+	virtual void makeImPossibleMove() {}
+
+
+	virtual bool isPossible() { return false; } //можно ли на эту клетку сходить - нет, нельзя, так как клетка неактивна
+	virtual bool isSelected() { return false; } //может ли эта клетка быть выделенной - нет, так как она не может иметь шашку
+	virtual bool isFree() { return true; }      //свободна ли эта клетка - да, всегда свободна, тк на ней ничего не стоит
+
+	virtual void setPost(CheckerHierarchy post) {}
+
+	//Функции получения цвета шашки и должности шашки возввращают исключения, 
+	//поскольку на клетке ничего не стоит по определению класса
+	virtual CheckerColor getCheckerColor() { return EXCEPTION_COLOR; }
+	virtual CheckerHierarchy getCheckerPost() { return EXCEPTION_POST; }
+};
+
+//Перечисление - для удобства, чтобы понимать если ли на этой клетке шашка или нет
+enum CHECKERS_HAVE {
+	HAS_CHECKER = true, HAS_NO_CHECKER = false
+};
+
+//Класс активная клетка - подкласс 
+//На такую клетку можно сходить и она может иметь шашку
+class ActiveCell : public Cell {
 	Checker* _checker;           //это указатель на шашку
 	bool _possibleMove = false;  //bool можно ли сходить на эту клетку или нельзя
 
